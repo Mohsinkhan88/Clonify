15e555b24dbf73c2dc625d6a50476d73777856a5
Abdullah Hamid <abdullahroshan17@gmail.com>
2014-02-09 15:22:44 +0500

    repository_model updated!
    file repository updated!

--- Clonify3/application/models/repository_model.php
+++ Clonify3/application/models/repository_model.php
@@ -40,20 +40,92 @@ class Repository_Model extends CI_Model
 		$date = date('Y-m-d H:i:s');
 		if ($dirPath=="")
 		{
-			$this->db->query("INSERT INTO repository_directory(repository_id) VALUES('$id')");
-			$newId = mysql_insert_id();
-			$this->db->query("INSERT INTO repository_file(file_name,directory_id,last_modified_time) VALUES('$fileName','$newId','$date')");
+			$newId;
+			if ($this->isExistDirectoryName($dirPath, $id))
+				{
+					$this->db->query("INSERT INTO repository_directory(repository_id) VALUES('$id')");
+					$newId = mysql_insert_id();
+					if ($this->isExistFileName($newId, $fileName))
+					{
+						$this->db->query("INSERT INTO repository_file(file_name,directory_id,last_modified_time) VALUES('$fileName','$newId','$date')");
+					}
+				}
+			else
+				{
+					$newId = $this->getDirectoryId($dirPath, $id);
+					if ($this->isExistFileName($newId, $fileName))
+					{
+						$this->db->query("INSERT INTO repository_file(file_name,directory_id,last_modified_time) VALUES('$fileName','$newId','$date')");
+					}
+				}
 		}
 		else
 		{
 			$newFileDir = $dirPath.'/';
-			$this->db->query("INSERT INTO repository_directory(repository_id,directory_name) VALUES('$id','$newFileDir')");
-			$newId = mysql_insert_id();
-			$this->db->query("INSERT INTO repository_file(file_name,directory_id,last_modified_time) VALUES('$fileName','$newId','$date')");
+			$newId;
+			if ($this->isExistDirectoryName($newFileDir, $id))
+				{
+					$this->db->query("INSERT INTO repository_directory(repository_id,directory_name) VALUES('$id','$newFileDir')");
+					$newId = mysql_insert_id();
+					if ($this->isExistFileName($newId, $fileName))
+					{
+						$this->db->query("INSERT INTO repository_file(file_name,directory_id,last_modified_time) VALUES('$fileName','$newId','$date')");
+					}
+				}
+			else
+				{
+					$newId = $this->getDirectoryId($dirPath.'/', $id);
+					if ($this->isExistFileName($newId, $fileName))
+					{
+						$this->db->query("INSERT INTO repository_file(file_name,directory_id,last_modified_time) VALUES('$fileName','$newId','$date')");
+					}
+				}
 		}
 		
 		
-	}	
+	}
+	//function to check if files already exist in the data base!	
+	function isExistDirectoryName($dirPath, $id)
+	{
+		$this->db->where('directory_name', $dirPath);
+		$this->db->where('repository_id', $id);
+		//$this->db->where('activated', $activated ? 1 : 0);
+
+		$query = $this->db->get('repository_directory');
+		if ($query->num_rows() > 0)
+		{ 
+			return  false;
+		}
+		return true;
+	}
 	
+	function getDirectoryId($dirPath, $id)
+	{
+		$this->db->where('directory_name', $dirPath);
+		$this->db->where('repository_id', $id);
+		//$this->db->where('activated', $activated ? 1 : 0);
+
+		$query = $this->db->get('repository_directory');
+		if ($query->num_rows() == 1)
+		{ 
+			$newData = $query->row();
+			return $newData->id;
+		}
+		return NULL;
+	}
+	
+	function isExistFileName($newId, $fileName)
+	{
+		$this->db->where('file_name', $fileName);
+		$this->db->where('directory_id', $newId);
+		//$this->db->where('activated', $activated ? 1 : 0);
+
+		$query = $this->db->get('repository_file');
+		if ($query->num_rows() > 0)
+		{ 
+			return  false;
+		}
+		return true;
+	}
 	
 }
