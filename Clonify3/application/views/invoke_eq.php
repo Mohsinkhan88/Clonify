5de69e4a3070cfb1828d3ed985d9ca4d643192da
khizerbajwa <khizerbajwa@gmail.com>
2014-02-09 15:27:59 +0500

    Initial Invocation Screen Validation
    Initial Invocation Screen Validation

--- Clonify3/application/views/invoke_eq.php
+++ Clonify3/application/views/invoke_eq.php
@@ -69,23 +69,6 @@ function SelectSort(SelList)
 
 <div id="wrapper">
 
-        <div id="sidebar">                    
-
-            <div class="sidenav">
-
-                <div class="sidebar-widget" style="margin: -1px 0 0 0;">
-                    <h5 class="title" style="margin-bottom:0">Ledt Side Bar</h5>
-                </div><!-- End .sidenav-widget -->
-
-                <div class="mainnav">
-                    <ul>
-                        <li><a href="#"><span class="icon16 icomoon-icon-stats-up"></span>Option 1</a></li>                                                
-                    </ul>
-                </div>
-            </div><!-- End sidenav -->            
-
-        </div><!-- End #sidebar -->
-
         <!--Body content-->
         <div id="content" class="clearfix">
             <div class="contentwrapper"><!--Content wrapper-->
